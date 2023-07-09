ebe3c9c3bf98d596a082e8ec1ce0709debbf36f9
Mohammadreza <mrrahbarnia@gmail.com>
2023-07-09 17:58:02 +0330

    +author one to many field in Post table,+Category table and category manytomany field in Post table

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post,Category
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    list_display = ('title','counted_views','status','published_date','created_date')
-    list_filter = ('status',)
+    list_display = ('title','author','counted_views','status','published_date','created_date')
+    list_filter = ('status','author')
     search_fields = ('title','content')
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
     
 
 
--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+        
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     image = models.ImageField(upload_to='blog',default='blog/default.jpg')
     # tag
-    # category
-    # author
+    category = models.ManyToManyField(Category)
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null = True)
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
